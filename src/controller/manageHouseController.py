4acbafd960e2fdb3139f85379059dc45ab049d46
Kientt <mrkientran.98@gmail.com>
2020-12-03 21:50:24 +0700

    master

--- src/controller/manageHouseController.py
+++ src/controller/manageHouseController.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 
 from config import client
 from src.controller import app
@@ -33,8 +34,7 @@ def get_initial_response():
 
 
 # tim kiem nha
-
-@app.route('/api/house/search', methods=['GET'])
+@app.route("/api/house/search", methods=['GET'])
 def get_all_house():
     args = request.args
     formSearch = {}
@@ -74,3 +74,70 @@ def create_house():
         # Add message for debugging purpose
         return "", 500
 
+@app.route("/delete-house/<id>", methods=['DELETE'])
+#xoa nha da ton tai
+def delete_house():
+    collection = db.house
+    try:
+        # Get the value which needs to be updated
+        # Get the value which needs to be updated
+        delete_house = collection.delete_one({"_id": ObjectId(id)})
+        if delete_house.deleted_count > 0:
+            res = responseData('SUCCESS', 'SUCCESS', '', "OK")
+            return dumps(res)
+        else:
+            # Resource Not found
+            return "", 404
+    except Exception as e:
+        # Error while trying to update the resource
+        # Add message for debugging purpose
+        return e, 500
+
+#api get one
+@app.route("/get-one-house/<id>", methods=['GET'])
+def findOneHouse(id):
+    try:
+        collection = db.house
+        house = collection.find_one({"_id": ObjectId(id)})
+        return  dumps({"data": house})
+    except Exception as e:
+        return e, 500
+
+@app.route("/update-one-house/<id>", methods=['POST'])
+def updateMovie(id):
+    collection = db.house
+    try:
+        # Get the value which needs to be updated
+        try:
+            body = ast.literal_eval(json.dumps(request.get_json()))
+        except:
+            # Bad request as the request body is not available
+            # Add message for debugging purpose
+            return "", 400
+        # Updating the user
+        # records_updated = collection.update_one({"_id": ObjectId(id)}, { "$set": body })
+        # Delete the house
+        deleted_house = collection.delete_one({"_id": ObjectId(id)})
+        # Check if resource is updated
+        if deleted_house.deleted_count > 0 :
+            # Prepare the response as resource is updated successfully
+            record_created = collection.insert(body)
+            res = responseData('SUCCESS', 'SUCCESS', '', record_created)
+            return dumps(res)
+        else:
+            # Bad request as the resource is not available to update
+            # Add message for debugging purpose
+            return "", 404
+    except Exception as e:
+        # Error while trying to update the resource
+        # Add message for debugging purpose
+        return e, 500
+
+
+def responseData(type, code, message, data):
+    return {
+        "type": type,
+        "code": code,
+        "message": message,
+        "data": data
+    }
